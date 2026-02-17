836bfc8e9d379a9cf4ea4b1b29bc07427e4533e0
Nikhil Misal <nikhil@nikhilmisal.com>
2026-02-17 22:21:54 +0530

    Fix catch-all route using regex for Express 5

--- server/index.js
+++ server/index.js
@@ -224,7 +224,7 @@ app.post('/api/audit', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../dist')));
 
-    app.get('/*', (req, res) => {
+    app.get(/(.*)/, (req, res) => {
         res.sendFile(path.join(__dirname, '../dist/index.html'));
     });
 }
